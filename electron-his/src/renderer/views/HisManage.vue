cca5533e0fa6cdd67a3d32d16a8285934939a4ad
LiYun <616852971@qq.com>
2019-05-16 18:27:16 +0800

    创建项目

---
+++ electron-his/src/renderer/views/HisManage.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "HisManage"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ electron-his/src/renderer/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div :class="[classNames + 'container']">
+        <div :class="[classNames + '-top']">
+            <h1>LYGG</h1>
+        </div>
+        <div :class="[classNames + '-logo']"></div>
+        <div :class="[classNames + '-content']"></div>
+    </div>
+</template>
+
+<script>
+    const classNames = 'his-login'
+    export default {
+        name: "Login",
+        data() {
+            return {
+                classNames: classNames
+            }
+        }
+    }
+</script>
