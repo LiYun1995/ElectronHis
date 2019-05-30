fa20c06bcade0ec0c3fce7bb1fdbc7acf54786b9
LiYun <616852971@qq.com>
2019-05-30 18:28:50 +0800

    5.30

---
+++ electron-his/src/renderer/components/manage/Content.vue
@@ -0,0 +1,17 @@
+<template>
+	<div :class="[classNames + '-container']">
+
+  </div>
+</template>
+
+<script>
+const classNames = 'his-manage-content'
+export default {
+	data() {
+		return {
+			classNames: classNames
+		}
+  },
+  methods:{}
+}
+</script>
--- electron-his/src/renderer/views/HisManage.vue
+++ electron-his/src/renderer/views/HisManage.vue
@@ -1,17 +1,15 @@
 <template>
-    <div style="width:100%;height:100%">
+    <div class="manage-main" style="width:100%;height:100%">
         <LeftTree></LeftTree>
+        <Content></Content>
     </div>
 </template>
 
 <script>
 import LeftTree from '@/components/manage/LeftTree'
+import Content from '@/components/manage/Content'
     export default {
         name: "HisManage",
-        components:{LeftTree}
+        components:{LeftTree,Content}
     }
 </script>
-
-<style scoped>
-
-</style>
